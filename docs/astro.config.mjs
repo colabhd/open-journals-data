960ee10defd884297ebd4535aac7d342341b70b2
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-12-16 13:46:22 -0300

    ajuste astro.config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://colabhd.github.io",
+  	base: "/open-journals-data/",
 	integrations: [
 		starlight({
 			title: 'My Docs',
